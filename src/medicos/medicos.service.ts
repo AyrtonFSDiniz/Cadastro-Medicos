05b360b1b2c809c4f8fac2d2c3e60ebf2bfc4ace
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2022-02-12 08:05:49 -0300

    Acertado códgo

--- src/medicos/medicos.service.ts
+++ src/medicos/medicos.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { map, Observable } from 'rxjs';
+import { lastValueFrom, map, Observable } from 'rxjs';
 import { Medicos } from '../model/medico.model';
 
 @Injectable()
@@ -13,135 +13,25 @@ export class MedicosService {
   ) {}
 
   async create(salvarMedico: Medicos): Promise<Medicos> {
-
     const cep = salvarMedico.cep;
+    /*const dado = this.httpService
+      .get(`https://viacep.com.br/ws/${{ cep }}/json`)
+      .toPromise();
+    const dados = dado && cep;*/
 
-    const dado = this.httpService
-    .get(`https://viacep.com.br/ws/${{cep}}/json`)
-    .toPromise();
-
+    const dado = await lastValueFrom(
+      this.httpService
+        .post(`https://viacep.com.br/ws/${{ cep }}/json`)
+        .pipe(map((resp) => resp.data)),
+    );
     const dados = dado && cep;
-
+    // const list = [];
+    // const dados = list.push(dado, cep);
     return await this.medicosModel.create(dados);
   }
 
 
-
-  /* getUsers(): Observable<AxiosResponse<User[]>> {
-    console.log('getUsers');
-    return this.httpService.get('http://localhost:3000/usersstatic').pipe(
-      map((response) => {
-        console.log(response.data);
-        return response.data;
-      }),
-    );
-  }
-
-  async getUsersPromise(): Promise<any> {
-    console.log('getUsersPromise');
-    const response = await this.httpService
-      .get('http://localhost:3000/usersstatic')
-      .toPromise();
-    return response.data;
-  }
-} */
-
-/* getQuery(query: string) {
-    this.httpService
-      .get('http://localhost:3001/get-query', {
-        params: {
-          key: query,
-        },
-      })
-      .subscribe(this.observer);
-  } */
-
-  /*import { Injectable, Inject } from '@nestjs/common'
-
-import githubAPI from '../../../config/githubAPI'
-import viacepAPI from '../../../config/viacepAPI'
-import IGithubData from '../dtos/IGithubData'
-import IAddress from '../dtos/IAddress'
-import { User } from '../models/User'
-import IUsersRepository from '../repositories/IUsersRepository'
-
-interface IGithubResponse {
-    total_count: number
-    incomplete_results: boolean
-    items: IGithubData[]
-}
-
-interface IRequest {
-    name: string
-    age: number
-    github_user: string
-    cep: string
-}
-
-@Injectable()
-class CreateUserService {
-
-    constructor(
-        @Inject('UsersRepository')
-        private repository: IUsersRepository
-    ) { }
-
-    public async execute({ name, age, github_user, cep }: IRequest): Promise<User> {
-        let responseGithub
-        try {
-            responseGithub = await githubAPI.get<IGithubResponse>(`search/users?q=${github_user}`)
-        } catch (err) {
-            throw Error('Unexpected error from GitHub!')
-        }
-
-        if (responseGithub.data.total_count === 0) {
-            throw Error('Github username does not exists!')
-        }
-
-        const {
-            login,
-            avatar_url,
-            repos_url
-        }: IGithubData = responseGithub.data.items[0]
-
-        if (cep.length !== 8) {
-            throw Error('CEP must have 8 digits!')
-        }
-
-        if (!RegExp('^[0-9]*$').test(cep)) {
-            throw Error('CEP has a non-numeric character!')
-        }
-
-        let responseViacep
-        try {
-            responseViacep = await viacepAPI.get<IAddress>(`${cep}/json/`)
-        } catch (err) {
-            throw Error('Unexpected error from ViaCEP!')
-        }
-
-        if (responseViacep.data.erro === true) {
-            throw Error('CEP does not exists!')
-        }
-
-        const user = await this.repository.create({
-            name,
-            age,
-            github_data: {
-                login,
-                avatar_url,
-                repos_url
-            },
-            address: responseViacep.data
-        })
-
-        return user
-    }
-
-}
-
-export default CreateUserService */
-
-/*import axios from 'axios';
+  /*import axios from 'axios';
 
 class Api {
 
@@ -162,7 +52,6 @@ class Address{
 
 //Digite o cep no campo numérico
 Api.getAddress('96030080').then(v => {console.log(v)});*/
-    
 
   async findAll(): Promise<Medicos[]> {
     return await this.medicosModel.findAll();
