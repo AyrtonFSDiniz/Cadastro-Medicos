3db6ebf20b4b7a680fc202d8e9ce26808f057c88
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2022-02-16 01:57:59 -0300

    Tentando rodar docker

--- src/medicos/medicos.module.ts
+++ src/medicos/medicos.module.ts
@@ -6,13 +6,17 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule.registerAsync({
-     useFactory: () => ({
-      timeout: 5000,
-    maxRedirects: 5,
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 5000,
+        maxRedirects: 5,
+      }),
     }),
-  }), SequelizeModule.forFeature([Medicos])],
+    SequelizeModule.forFeature([Medicos]),
+  ],
   controllers: [MedicosController],
   providers: [MedicosService],
+  exports: [SequelizeModule.forFeature([Medicos])],
 })
 export class MedicosModule {}
